01f2e762dfb5ac5c1cba4e697b6884481846549c
jProgr <8537691@gmail.com>
2020-01-11 14:02:44 -0600

    Window and scene base call main controller

--- githubfollowers/SceneDelegate.swift
+++ githubfollowers/SceneDelegate.swift
@@ -1,23 +1,17 @@
-//
-//  SceneDelegate.swift
-//  githubfollowers
-//
-//  Created by Cristóbal on 10/01/20.
-//  Copyright © 2020 jProgr. All rights reserved.
-//
-
 import UIKit
 
-class SceneDelegate: UIResponder, UIWindowSceneDelegate {
-
+class SceneDelegate: UIResponder, UIWindowSceneDelegate
+{
     var window: UIWindow?
 
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions)
+    {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
 
-    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard let _ = (scene as? UIWindowScene) else { return }
+        window = UIWindow(frame: windowScene.coordinateSpace.bounds)
+        window?.windowScene = windowScene
+        window?.rootViewController = ViewController()
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
@@ -47,7 +41,4 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
-
-
 }
-
--- githubfollowers/ViewController.swift
+++ githubfollowers/ViewController.swift
@@ -1,20 +1,11 @@
-//
-//  ViewController.swift
-//  githubfollowers
-//
-//  Created by Cristóbal on 10/01/20.
-//  Copyright © 2020 jProgr. All rights reserved.
-//
-
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        view.backgroundColor = .systemPink
     }
-
-
 }
-
