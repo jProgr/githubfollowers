d8786348a8039c23a59f0d94cbfe510a73ae6189
jProgr <8537691@gmail.com>
2020-03-15 11:51:57 -0600

    Minor refactoring

--- githubfollowers/Controllers/FollowerListViewController.swift
+++ githubfollowers/Controllers/FollowerListViewController.swift
@@ -39,7 +39,8 @@ class FollowerListViewController: UIViewController
 
     func getFollowers()
     {
-        NetworkManager.shared.getFollowers(for: username, page: 1) { result in
+        NetworkManager.shared.getFollowers(for: username, page: 1) { [weak self] result in
+            guard let self = self else { return }
             switch result
             {
                 case .success(let followers):
@@ -53,25 +54,10 @@ class FollowerListViewController: UIViewController
 
     func configureCollectionView()
     {
-        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: createThreeColumnFlowLayout())
+        collectionView = UICollectionView(frame: view.bounds, collectionViewLayout: UIHelper.createThreeColumnFlowLayout(in: view))
         view.addSubview(collectionView)
         collectionView.backgroundColor = .systemBackground
         collectionView.register(FollowerCell.self, forCellWithReuseIdentifier: FollowerCell.reuseID)
-    }
-
-    func createThreeColumnFlowLayout() -> UICollectionViewFlowLayout
-    {
-        let width = view.bounds.width
-        let padding: CGFloat = 12
-        let minItemSpacing: CGFloat = 10
-        let availableWidth = width - (padding * 2) - (minItemSpacing * 2)
-        let itemWidth = availableWidth / 3
-
-        let flowLayout = UICollectionViewFlowLayout()
-        flowLayout.sectionInset = UIEdgeInsets(top: padding, left: padding, bottom: padding, right: padding)
-        flowLayout.itemSize = CGSize(width: itemWidth, height: itemWidth + 40)
-
-        return flowLayout
     }
 
     func configureDataSource()
